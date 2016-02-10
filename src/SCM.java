c2663e159e00b71e82686fb6e04e5f60483eccb0
Braydprest <Brayden.prestwich@gmail.com>
2016-02-09 17:41:59 -0700

    check this out

---
+++ src/SCM.java
@@ -0,0 +1,30 @@
+
+public class SCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		
+		System.out.println("Get hub is easy... or is it?");
+		System.out.println("Get hub is easy... or is it?");
+		
+	}
+	
+	
+	public static void donothing(){
+		int i = 0;
+		
+		for (i = 0; i < 10; --i){
+			
+			System.out.println("Hello World");
+			
+		}
+		
+	}
+	
+	
+	System.out.println("say something im giveing up on you");
+	
+
+}
